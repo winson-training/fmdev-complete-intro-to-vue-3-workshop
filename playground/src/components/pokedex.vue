e471dd1a6faffe0575efa2b8cc64b1f6132c14c8
Winson Tang <winsontang@gmail.com>
2024-11-24 13:30:01 -0800

    feature: update pokedex to be almost fully composit

--- playground/src/components/pokedex.vue
+++ playground/src/components/pokedex.vue
@@ -19,7 +19,12 @@ export default {
       "https://pokeapi.co/api/v2/pokemon?limit=151"
     ).then((response) => response.json());
 
+    const changeRegionName = () => {
+      regionName.value = "Johto";
+    };
+
     return {
+      changeRegionName,
       regionName,
       pokedex,
       regionNameAllCaps,
@@ -31,21 +36,8 @@ export default {
     console.log("beforeCreate");
     console.log(this.pokedex);
   },
-  computed: {
-    regionNameLowerCase() {
-      return this.regionName.toLowerCase();
-    },  
-  },
-  methods: {
-    changeRegionName() {
-      this.regionName = "Johto";
-    },
-  },
-  created() {
-    console.log("created");
-    console.log(this.pokedex);
-    console.log(this.regionName);
-  },
+
+
 };
 </script>
 
@@ -53,7 +45,6 @@ export default {
     <h2>{{ regionName }}</h2>
     <h3>{{regionNameAllCaps}}</h3>
     <h3>{{elementTypeAllCaps}}</h3>
-    <h4>{{regionNameLowerCase}}</h4>
     <button @click="changeRegionName">Change Region Name</button>
   <pre>{{ pokedex }}</pre>
 </template>
